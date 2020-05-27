6501cbca5b25cc7133d715bed43bbc0da969e209
stacey2nz <staceywatson1987@gmail.com>
2020-05-27 18:58:48 +1200

    Third new line of code addded.

--- PartD/src/PartD/PartDMain.java
+++ PartD/src/PartD/PartDMain.java
@@ -5,6 +5,7 @@ public class PartDMain {
 	public static void main(String[] args) {
 			System.out.println("Hi, my name is Stacey");
 			System.out.println("My favourite colour is purple");
+			System.out.println("My favourite mode of transport is my car");
 	}
 
 }
