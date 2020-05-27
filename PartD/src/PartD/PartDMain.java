458fdcc8275bdbb2fa66ec8f16229a33edcd656a
stacey2nz <staceywatson1987@gmail.com>
2020-05-27 18:50:36 +1200

    Second commit

--- PartD/src/PartD/PartDMain.java
+++ PartD/src/PartD/PartDMain.java
@@ -4,6 +4,7 @@ public class PartDMain {
 
 	public static void main(String[] args) {
 			System.out.println("Hi, my name is Stacey");
+			System.out.println("My favourite colour is purple");
 	}
 
 }
